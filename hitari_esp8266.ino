57ec35839f31fa3b383f4a5a0d28910d66ad0402
石川大輔 <daaaisuke15176266@gmail.com>
2017-12-05 11:43:46 +0900

    stash

--- hitari_esp8266.ino
+++ hitari_esp8266.ino
@@ -1,213 +1,207 @@
-#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <EEPROM.h>
 #include <ESP8266WiFi.h>
+#include <TimeLib.h>
 #include "RTClib.h"
 extern "C" {
 #include "user_interface.h"
 }
 
-
-const int LEDPIN = 12;//LEDデジタルピン
+const int RTCPIN = 0;
+const int LEDPIN = 12;
 const int SPARKPIN = 5;
-const int RELAY_PIN = 13;///リードリレー制御ピン
+const int RELAY_PIN = 13;
 
-const int NUMPIXELS = 4;//LED個数
-const int FALL_LIGHT_NUM = 3;
-const int SPARK_LIGHT_NUM = 8;
+const int NUMPIXELS = 5;
+const int SPARK_LIGHT_NUM = 7;
 
-const int BOOTTIME = 21;//起動時間
+const int BOOTTIME = 22;//起動時間
 const int SLEEPTIME = 2;//スリープ時間
 
 const int DATASIZE = 1683;//EEPROMデータ数
 
 
-
-DateTime now;
+DateTime rnow;
+DateTime anow;
 RTC_DS1307 rtc;
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_RGB + NEO_KHZ800);
-Adafruit_NeoPixel sparks = Adafruit_NeoPixel(SPARK_LIGHT_NUM, SPARKPIN, NEO_RGB + NEO_KHZ800);
 
 bool bLux = true;//連続起動制御用bool
 
-void setup() {
-  Serial.begin(115200);//シリアル通信スタート
-  wifi_stop();//wifi機能stop
-  randomSeed(analogRead(5));
-  ledSetup(0);
-  rtcSetup();
-  showDate();
-  EEPROM.begin(DATASIZE);
-  //    if (now.hour() >= SLEEPTIME/*2*/ && now.hour() < BOOTTIME/*21*/) { //スリープさせるかどうか判定
-  //    スリープ時間だったら
-  //      setAlarm();
-  //    }
 
+
+void setup() {
+  Serial.begin(115200);//===削除===
+  wifi_stop();
+  EEPROM.begin(DATASIZE + 7);
+  rtcSetup();
+  if (hour() >= SLEEPTIME/*2*/ && hour() < BOOTTIME/*22*/) { //スリープさせるかどうか判定
+    Serial.println("setupでスリープ");
+    //    setAlarm();
+  }
+
+  randomSeed(analogRead(5));
+  ledSetup();
 }
 
-
 void loop() {
-  showDate();
+  //====================================================================================
 
-  int bright;
+  if (hour() == SLEEPTIME) {
+    Serial.println("loopでスリープ");
+    //    setAlarm();
+  }
+
+  //====================================================================================
+
   int Lux = system_adc_read();
   Serial.println(Lux);
-  if (Lux > 600 && bLux) { //部屋が暗くなったら
-    int strand = random(5);
-    setFirstDelay(strand);//暗くなってから光り始めるまでの時間
-    int int_rand = random(100);//インターバル時間決定用乱数1
-    int sec_rand = random(20) * 1000;//インターバル時間決定用乱数2
-    for (int i = 0; i < DATASIZE - FALL_LIGHT_NUM * 3; i += 3) {//サブのLED用のデータ以外ループ
-      //      lightLED(int_rand, sec_rand, i);
-      lightLED(35, 0, i);
+
+  if (Lux > 600 && bLux) {//暗くなったら
+    digitalWrite(RELAY_PIN, HIGH);//リードリレーON
+    delay(5);
+    for (int i = 0; i < NUMPIXELS; i++) {//全LEDを落とす
+      pixels.setPixelColor(i, 0, 0, 0);
     }
-    //    fallLight();
+    for (int i = 0; i < SPARK_LIGHT_NUM; i++) {//全LEDを落とす
+      sparks.setPixelColor(i, 0, 0, 0);
+    }
+    pixels.show();
+    sparks.show();
+
+    //---------------------
+    int first_wait_rand = random(4, 9);
+    delay(first_wait_rand * 1000);//暗くなってから光り始めるまでの時間
+    int bright_time_rand1 = random(100);//インターバル時間決定用乱数1
+    int bright_time_rand2 = random(20) * 1000;//インターバル時間決定用乱数2
+    //---------------------
+
+
+    for (int i = 0; i < DATASIZE - (NUMPIXELS - 1) * 3; i += 3) { //サブのLED用のデータ以外ループ
+      lightLED(bright_time_rand1, bright_time_rand2, i);
+    }
+    fallLight();
 
     bLux = false;
+    digitalWrite(RELAY_PIN, LOW);//リードリレーON
   } else if (Lux < 600 && !bLux) {
     bLux = true;
   }
   delay(100);//照度センサタイミング
-  //    if (now.hour() == SLEEPTIME) {//スリープ時間になったら
-  //      setAlarm();
-  //    }
-  pixels.setBrightness(0);
-  sparks.setBrightness(0);
-  for (int i = 0; i < NUMPIXELS; i++) {//全LEDを落とす
-    pixels.setPixelColor(i, 0, 0, 0);
-  }
-  for (int i = 0; i < SPARK_LIGHT_NUM; i++) {//全LEDを落とす
-    sparks.setPixelColor(i, 0, 0, 0);
-  }
-  pixels.show();
-  sparks.show();
+  showDate();
+
 }
 
 
 //========================================================
-void ledSetup(int bright) {//リードリレーON&&LED消灯
+void ledSetup() {//リードリレーON&&LED消灯
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);//リードリレーON
-  pixels.begin();
-  sparks.begin();
-  pixels.setBrightness(bright);
-  sparks.setBrightness(bright);
-  for (int i = 0; i < NUMPIXELS; i++) {//全LEDを落とす
-    pixels.setPixelColor(i, 0, 0, 0);
-  }
-  for (int i = 0; i < SPARK_LIGHT_NUM; i++) {//全LEDを落とす
-    sparks.setPixelColor(i, 0, 0, 0);
-  }
-  pixels.show();
-  sparks.show();
+  
 }
 
 
 void rtcSetup() {
+  pinMode(RTCPIN, OUTPUT);
   rtc.begin();
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));//初回のみ
+  //  EEPROM.write(1683, 1);
+  //  EEPROM.commit();
+  if (EEPROM.read(1683) == 1) {
+    digitalWrite(RTCPIN, HIGH);//RTC電源ON
+    delay(100);
+    rnow = rtc.now();
+  } else {
+    rnow = DateTime(EEPROM.read(1684), EEPROM.read(1685), EEPROM.read(1686), EEPROM.read(1687), EEPROM.read(1688), EEPROM.read(1689)) + TimeSpan(0, 1, 0, 0);
+  }
+  setTime(rnow.hour(), rnow.minute(), rnow.second(), rnow.day(), rnow.month(), rnow.year());
+  showDate();
+  rtc.writeSqwPinMode(OFF);
+  digitalWrite(RTCPIN, LOW);//RTC電源OFF
+
 }
 
 
-
-
-
-void setAlarm() {
-  now = rtc.now();
-  Serial.println(now.day());
-  Serial.println(now.hour());
-  Serial.println(now.minute());
-  Serial.println(now.second());
-  while (now.hour() != BOOTTIME) {//bootタイムまでの時間を計算
-    now = now + TimeSpan(0, 1, 0, 0);
+void setAlarm() {//setup→時間未定,loop→SLEEPTIME(2時)
+  rnow = DateTime(year(), month(), day(), hour(), minute(), second());//スリープ時の時刻を記録しておく
+  anow = rnow;
+  //スリープまでの時間を探る
+  while (anow.hour() != BOOTTIME) {//22時
+    anow = anow + TimeSpan(0, 1, 0, 0);
   }
 
-  digitalWrite(RELAY_PIN, LOW);//リードリレーOFF
-  DateTime Alarm(now.year(), now.month(), now.day(), now.hour(), 00, 00);
-  TimeSpan diff = Alarm - rtc.now();
+  DateTime Alarm(anow.year(), anow.month(), anow.day(), anow.hour(), 00, 00);
+  Serial.print(Alarm.year());
+  Serial.print(Alarm.month());
+  Serial.print(Alarm.day());
+  Serial.print(Alarm.hour());
+  Serial.print(Alarm.minute());
+  Serial.println(Alarm.second());
+  TimeSpan diff = Alarm - rnow;
+  Serial.print(diff.hours());
+  Serial.print("/");
+  Serial.print(diff.minutes());
+  Serial.print("/");
+  Serial.println(diff.seconds());
+
   if (diff.hours() == 0) {
-    unsigned int diffTS = diff.totalseconds() * 1000 * 1000 / 24 - 1 * 1000 * 1000;//起きる時刻までの秒数を指定
-    Serial.println(diffTS);
+    //            unsigned int diffTS = diff.totalseconds() * 1000 * 1000 / 24 - 1 * 1000 * 1000;//待ちの一秒を引く//動かない======================
+    pinMode(0, INPUT_PULLUP);
+    pinMode(5, INPUT_PULLUP);
     pinMode(12, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
     Serial.print(" - LAST_POWER_DOWN -");
     delay(1000);
-    ESP.deepSleep(diffTS , WAKE_RF_DEFAULT);
+    ESP.deepSleep(3600 * 1000 * 1000 , WAKE_RF_DISABLED);//===調査===
   } else {
     pinMode(12, INPUT_PULLUP);//使わないピンは省エネ化
+    //        if (EEPROM.read(1683) != 0) {
+    //          EEPROM.write(1683, 0 );
+    //        }
+
+    EEPROM.write(1684, year() % 100 );
+    EEPROM.write(1685, month() );
+    EEPROM.write(1686, day() );
+    EEPROM.write(1687, hour() );
+    EEPROM.write(1688, minute() );
+    EEPROM.write(1689, second() );
+    EEPROM.commit();
     Serial.print(" - POWER_DOWN -");
     delay(1000);
-    ESP.deepSleep(3600 * 1000 * 1000 , RF_DISABLED);
+    ESP.deepSleep(3600 * 1000 * 1000 , WAKE_RF_DISABLED);//====調査===
   }
 
-}
-
-void setFirstDelay(int r) {
-  switch (r) {
-    case 0:
-      delay(8000);
-      break;
-    case 1:
-      delay(9000);
-      break;
-    case 2:
-      delay(10000);
-      break;
-    case 3:
-      delay(11000);
-      break;
-    case 4:
-      delay(12000);
-      break;
-  }
 }
 
 void Interval(int r1, int r2) {
   if (r1 < 15) {
-    delay((30000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
+    delay((50000 + r2) / (DATASIZE / 3 - (NUMPIXELS - 1)) / 2);
   } else if (r1 >= 15 && r1 < 50) {
-    delay((50000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
+    delay((60000 + r2) / (DATASIZE / 3 - (NUMPIXELS - 1)) / 2);
   } else if (r1 >= 50 && r1 < 70) {
-    delay((70000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
+    delay((70000 + r2) / (DATASIZE / 3 - (NUMPIXELS - 1)) / 2);
   } else if (r1 >= 70 && r1 < 85) {
-    delay((90000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
-  } else if (r1 >= 85 && r1 < 95) {
-    delay((110000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
+    delay((80000 + r2) / (DATASIZE / 3 - (NUMPIXELS - 1)) / 2);
   } else {
-    delay((130000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3);
+    delay((90000 + r2) / (DATASIZE / 3 - (NUMPIXELS - 1)) / 2);
   }
 }
 
-void HarfInterval(int r1, int r2) {
-  if (r1 < 15) {
-    delay((30000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  } else if (r1 >= 15 && r1 < 50) {
-    delay((50000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  } else if (r1 >= 50 && r1 < 70) {
-    delay((70000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  } else if (r1 >= 70 && r1 < 85) {
-    delay((90000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  } else if (r1 >= 85 && r1 < 95) {
-    delay((110000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  } else {
-    delay((130000 + r2) / (DATASIZE - (NUMPIXELS - 4) * 3) * 3 / 2);
-  }
-}
 void lightLED(int int_rand, int sec_rand, int i) {
-  int bright =  (EEPROM.read(i) + EEPROM.read(i + 1) + EEPROM.read(i + 2) * 10) / 2 - 30;
+  int bright =  (EEPROM.read(i) + EEPROM.read(i + 1) + EEPROM.read(i + 2) * 10) / 2 - 10;
   if (bright > 255) {
     bright = 255;
   }
-  Serial.println(bright);
+  if (bright < 10) {
+    bright += 5;
+  }
   pixels.setBrightness(bright);
   pixels.setPixelColor(0, EEPROM.read(i), EEPROM.read(i + 1), EEPROM.read(i + 2));
 
-
   if (bright > 90) { //spark処理
-    sparks.setBrightness(20);
-    Serial.println("発火!!!");
+    sparks.setBrightness(60);
     int r[SPARK_LIGHT_NUM];
     for (int j = 0; j < sizeof(r); j++) {
-      r[j] = random(0, 4);
+      r[j] = random(0, 5);
       if (r[j] == 3) {
         sparks.setPixelColor(j, EEPROM.read(i), EEPROM.read(i + 1), EEPROM.read(i + 2));
       }
@@ -215,25 +209,28 @@ void lightLED(int int_rand, int sec_rand, int i) {
   }
   pixels.show();
   sparks.show();
-  HarfInterval(int_rand, sec_rand);
+  Interval(int_rand, sec_rand);
   for (int j = 0; j < SPARK_LIGHT_NUM; j ++) {
     sparks.setPixelColor(j, 0, 0, 0);
   }
   sparks.show();
-  HarfInterval(int_rand, sec_rand);
+  Interval(int_rand, sec_rand);
 }
 
 void fallLight() {
   pixels.setPixelColor(0, 0, 0, 0);//mainLEDを消灯
   pixels.show();
-  for (int i = 1; i < NUMPIXELS - 3; i++) {//1,2,3,4
-    int x = (-(NUMPIXELS - 3) + i) * 3 + 1 + DATASIZE; //-12,-9,-6,-3
-    int bright = (EEPROM.read(x) + EEPROM.read(x + 1) + EEPROM.read(x + 2) * 10) / 2 + 30;
+  for (int i = 1; i < NUMPIXELS; i++) {
+    int x = DATASIZE - (NUMPIXELS - i) * 3;
+    int bright = (EEPROM.read(x) + EEPROM.read(x + 1) + EEPROM.read(x + 2)) / 3;
     if (bright > 255) {
       bright = 255;
     }
+    if (bright < 10) {
+      bright += 10;
+    }
     pixels.setBrightness(bright);
-    pixels.setPixelColor(i , EEPROM.read(x - 1), EEPROM.read(x), EEPROM.read(x + 1));
+    pixels.setPixelColor(i , EEPROM.read(x), EEPROM.read(x + 1), EEPROM.read(x + 2));
     pixels.show();
     delay(25);
     pixels.setPixelColor(i, 0, 0, 0);
@@ -243,18 +240,17 @@ void fallLight() {
 
 
 void showDate() {
-  now = rtc.now();
-  Serial.print(now.year());
+  Serial.print(year());
   Serial.print("/");
-  Serial.print(now.month());
+  Serial.print(month());
   Serial.print("/");
-  Serial.print(now.day());
+  Serial.print(day());
   Serial.print("/");
-  Serial.print(now.hour());
+  Serial.print(hour());
   Serial.print("/");
-  Serial.print(now.minute());
+  Serial.print(minute());
   Serial.print("/");
-  Serial.println(now.second());
+  Serial.println(second());
 }
 
 void wifi_stop() {
